c818bc31f16be81db2cecb3dda516be662e851af
IAmEatingAnApple <70452799+IAmEatingAnApple@users.noreply.github.com>
2020-09-30 14:37:55 +0300

    Add files via upload

--- dos.py
+++ dos.py
@@ -1,10 +1,10 @@
 import requests
-from colorama import Fore, Style
+from colorama import Fore, Style, Back
 site = input("Введите сайт: ")
 i = 1
 while True:
 	response = requests.get(f"{site}").text
-	print(response)
+	print(Fore.YELLOW + response)
 	print(Fore.GREEN + f"Пакет послан {i}")
 	print(Style.RESET_ALL)
 	i = i + 1
